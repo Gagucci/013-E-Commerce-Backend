941f585e2378e7e1b5193bba35553b5d4d1d60ea
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-10-03 20:45:46 -0700

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,16 +32,42 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
+  try {
+    const ctgData = await Category.create(req.body);
+    res.status(200).json(ctgData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const ctgData = await Category.update(req.body, { where: { id: req.params.id } });
+    if (!ctgData) {
+      res.status(404).json({ message: 'No Category exists on that id' });
+      return;
+    }
+    res.status(200).json(ctgData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const ctgData = await Category.destroy({ where: { id: req.params.id } });
+    if (!ctgData) {
+      res.status(404).json({ message: 'No Category exists on that id' });
+      return;
+    }
+    res.status(200).json(ctgData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
