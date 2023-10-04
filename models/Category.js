0e0be816704205ab89e42051ebd4eb6240068546
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-10-03 20:11:56 -0700

    made product and category models

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      primaryKey: true, // set as primary key
+      allowNull: false, // doesn't allow null values
+      autoIncrement: true // uses auto increment
+    },
+    category_name: {
+      type: DataTypes.STRING, // string
+      allowNull: false // doesn't allow null values
+    }
   },
   {
     sequelize,
